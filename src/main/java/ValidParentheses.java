9b088280e5463da88ac72a5008ad5213ad0eca3c
zhangsw <zhangsw1412.cpp@gmail.com>
2017-09-19 11:02:26 +0800

    ValidParentheses

---
+++ src/main/java/ValidParentheses.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created by zhangsw on 2017/9/19.
+ */
+public class ValidParentheses
+{
+	class Solution
+	{
+		//自己完成的版本
+		public boolean isValid(String s)
+		{
+			if (s.length() % 2 == 1)
+			{
+				return false;
+			}
+			Stack<Character> stack = new Stack<>();
+			Map<Character, Character> map = new HashMap<>();
+			map.put(')', '(');
+			map.put(']', '[');
+			map.put('}', '{');
+			char[] chars = s.toCharArray();
+			for (int i = 0; i < chars.length; ++i)
+			{
+				if (chars[i] == '(' || chars[i] == '[' || chars[i] == '{')
+				{
+					stack.push(chars[i]);
+				}
+				else
+				{
+					if ((!stack.empty()) && (map.get(chars[i]) != stack.pop()))
+					{
+						return false;
+					}
+				}
+			}
+			return stack.empty() ? true : false;
+		}
+	}
+}
