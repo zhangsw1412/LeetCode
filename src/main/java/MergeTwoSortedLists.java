497ea5f505d501ead95e2ad30c9250e5d6dd3914
zhangsw <zhangsw1412.cpp@gmail.com>
2017-09-19 19:29:24 +0800

    MergeTwoSortedLists递归版本

--- src/main/java/MergeTwoSortedLists.java
+++ src/main/java/MergeTwoSortedLists.java
@@ -24,7 +24,30 @@ public class MergeTwoSortedLists
 
 	class Solution
 	{
+		//递归版本
+		public ListNode mergeTwoLists(ListNode l1, ListNode l2)
+		{
+			if (l1 == null)
+			{
+				return l2;
+			}
+			if (l2 == null)
+			{
+				return l1;
+			}
+			if (l1.val <= l2.val)
+			{
+				l1.next = mergeTwoLists(l1.next, l2);
+				return l1;
+			}
+			else
+			{
+				l2.next = mergeTwoLists(l1, l2.next);
+				return l2;
+			}
+		}
 		//自己完成的版本
+		/*
 		public ListNode mergeTwoLists(ListNode l1, ListNode l2)
 		{
 			ListNode head = new ListNode(0);
@@ -53,5 +76,6 @@ public class MergeTwoSortedLists
 			}
 			return head.next;
 		}
+		*/
 	}
 }
