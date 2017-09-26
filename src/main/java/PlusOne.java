45f763bd08e2c4ddd7d07ea7b87b89372d7f7538
zhangsw <zhangsw_1412@foxmail.com>
2017-09-26 21:09:43 +0800

    Plus One

---
+++ src/main/java/PlusOne.java
@@ -0,0 +1,24 @@
+/**
+ * Created by zhangsw on 2017/9/26.
+ */
+public class PlusOne
+{
+	class Solution
+	{
+		public int[] plusOne(int[] a)
+		{
+			for (int i = a.length - 1; i >= 0; --i)
+			{
+				if (a[i] < 9)
+				{
+					++a[i];
+					return a;
+				}
+				a[i] = 0;
+			}
+			int[] res = new int[a.length + 1];
+			res[0] = 1;
+			return res;
+		}
+	}
+}
