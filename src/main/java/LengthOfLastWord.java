352c091a5c23a23bf268a50b044d77f0dd5c4886
zhangsw <zhangsw_1412@foxmail.com>
2017-09-26 16:51:30 +0800

    Length of Last Word

---
+++ src/main/java/LengthOfLastWord.java
@@ -0,0 +1,21 @@
+/**
+ * Created by zhangsw on 2017/9/26.
+ */
+public class LengthOfLastWord
+{
+	class Solution
+	{
+		public int lengthOfLastWord(String s)
+		{
+			if (s == null || s.length() == 0)
+			{
+				return 0;
+			}
+			String[] strings = s.trim().split(" ");
+			return strings == null ? 0 : strings[strings.length - 1].length();
+
+			//一个强大的版本，只要一句话就行
+			//return s.trim().length()-s.trim().lastIndexOf(" ")-1;
+		}
+	}
+}
