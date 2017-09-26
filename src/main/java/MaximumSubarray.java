27d11e4335a6cbb5e0613cea4e1f9f1113558a9d
zhangsw <zhangsw_1412@foxmail.com>
2017-09-26 16:33:25 +0800

    MaximumSubarray

---
+++ src/main/java/MaximumSubarray.java
@@ -0,0 +1,26 @@
+/**
+ * Created by zhangsw on 2017/9/26.
+ */
+public class MaximumSubarray
+{
+	class Solution
+	{
+		public int maxSubArray(int[] a)
+		{
+			int sum = a[0], temp = 0;
+			for (int i = 0, len = a.length; i < len; ++i)
+			{
+				temp += a[i];
+				if (temp > sum)
+				{
+					sum = temp;
+				}
+				if (temp < 0)
+				{
+					temp = 0;
+				}
+			}
+			return sum;
+		}
+	}
+}
