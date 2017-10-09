1d6130f7ef44457354efe2073a3113107484336f
zhangsw <zhangsw_1412@foxmail.com>
2017-10-09 16:05:26 +0800

    Sqrt(x)作弊版

---
+++ src/main/java/Sqrt_x.java
@@ -0,0 +1,14 @@
+/**
+ * Created by zhangsw on 2017/10/9.
+ */
+public class Sqrt_x
+{
+	class Solution
+	{
+		//有点作弊的版本...
+		public int mySqrt(int a)
+		{
+			return (int) Math.sqrt(a);
+		}
+	}
+}
