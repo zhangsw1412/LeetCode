115c43a634494bcc060257388bae98d0ca8b71e3
zhangsw <zhangsw_1412@foxmail.com>
2017-10-09 16:15:18 +0800

    Sqrt(x)

--- src/main/java/Sqrt_x.java
+++ src/main/java/Sqrt_x.java
@@ -5,10 +5,22 @@ public class Sqrt_x
 {
 	class Solution
 	{
+		//这个方法仍然有点作弊，正常的解法是用牛顿开根法
+		public int mySqrt(int a)
+		{
+			int result = 0;
+			while (Math.pow(result, 2) <= a)
+			{
+				++result;
+			}
+			return result - 1;
+		}
 		//有点作弊的版本...
+		/*
 		public int mySqrt(int a)
 		{
 			return (int) Math.sqrt(a);
 		}
+		*/
 	}
 }
