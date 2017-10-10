de40f6c8d6cd83247ba11638d1b13b08a745c7c2
zhangsw <zhangsw_1412@foxmail.com>
2017-10-10 16:07:47 +0800

    Maximum Depth of Binary Tree.

---
+++ src/main/java/leetcode/easy/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,33 @@
+package leetcode.easy;
+
+/**
+ * Created by zhangsw on 2017/10/10.
+ */
+public class MaximumDepthOfBinaryTree
+{
+	class TreeNode
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x)
+		{
+			val = x;
+		}
+	}
+
+	class Solution
+	{
+		public int maxDepth(TreeNode root)
+		{
+			if (root == null)
+			{
+				return 0;
+			}
+			int left = maxDepth(root.left);
+			int right = maxDepth(root.right);
+			return left > right ? left + 1 : right + 1;
+		}
+	}
+}
