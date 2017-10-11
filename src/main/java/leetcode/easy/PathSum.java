67f9d4c0f2cdcf915aae6192f290c737ad8345f6
zhangsw <zhangsw_1412@foxmail.com>
2017-10-11 10:59:32 +0800

    Path Sum.

---
+++ src/main/java/leetcode/easy/PathSum.java
@@ -0,0 +1,39 @@
+package leetcode.easy;
+
+/**
+ * Created by zhangsw on 2017/10/11.
+ */
+public class PathSum
+{
+	/**
+	 * Definition for a binary tree node.
+	 */
+	class TreeNode
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x)
+		{
+			val = x;
+		}
+	}
+
+	class Solution
+	{
+		public boolean hasPathSum(TreeNode root, int sum)
+		{
+			if (root == null)
+			{
+				return false;
+			}
+			sum -= root.val;
+			if (sum == 0 && root.left == null && root.right == null)
+			{
+				return true;
+			}
+			return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+		}
+	}
+}
