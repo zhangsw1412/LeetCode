dafdb37b5d381818005da27d175cc7b7e41144a9
zhangsw <zhangsw_1412@foxmail.com>
2017-10-11 10:36:08 +0800

    Balanced Binary Tree一次遍历的方法.

--- src/main/java/leetcode/easy/BalancedBinaryTree.java
+++ src/main/java/leetcode/easy/BalancedBinaryTree.java
@@ -43,5 +43,34 @@ public class BalancedBinaryTree
 			int right = depth(node.right);
 			return Math.abs(left - right) <= 1 && balanced(node.left) && balanced(node.right);
 		}
+
+		//一种复杂度为O(n)的方法，采用深度优先遍历，如果不平衡就返回一个标识，否则返回树高
+		/*
+		private static final int UNBALANCED = -99;
+
+		public boolean isBalanced(TreeNode root)
+		{
+			if (root == null)
+			{
+				return true;
+			}
+			return getHeight(root) != UNBALANCED;
+		}
+
+		private int getHeight(TreeNode root)
+		{
+			if (root == null)
+			{
+				return -1;
+			}
+			int l = getHeight(root.left);
+			int r = getHeight(root.right);
+			if (l == UNBALANCED || r == UNBALANCED || Math.abs(l - r) > 1)
+			{
+				return UNBALANCED;
+			}
+			return 1 + Math.max(l, r);
+		}
+		*/
 	}
 }
