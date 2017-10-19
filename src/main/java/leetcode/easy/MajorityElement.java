511df8ef0cf47f307fc7992e51963e1ecebefe5d
zhangsw <zhangsw_1412@foxmail.com>
2017-10-19 16:29:00 +0800

    Majority Element.复杂度O(n)解法.

--- src/main/java/leetcode/easy/MajorityElement.java
+++ src/main/java/leetcode/easy/MajorityElement.java
@@ -9,11 +9,31 @@ public class MajorityElement
 {
 	class Solution
 	{
+		//Moore voting algorithm
+		public int majorityElement(int[] a)
+		{
+			int counter = 0, majority = a[0];
+			for (int i = 0; i < a.length; ++i)
+			{
+				if (counter == 0)
+				{
+					majority = a[i];
+					++counter;
+				}
+				else
+				{
+					counter += (a[i] == majority) ? 1 : -1;
+				}
+			}
+			return majority;
+		}
 		//这种写法有点偷懒
+		/*
 		public int majorityElement(int[] a)
 		{
 			Arrays.sort(a);
 			return a[a.length / 2];
 		}
+		*/
 	}
 }
