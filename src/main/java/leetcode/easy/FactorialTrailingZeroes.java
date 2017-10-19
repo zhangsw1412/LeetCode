fb1c048a22d22a827e2c0791a9ce69d969b36163
zhangsw <zhangsw_1412@foxmail.com>
2017-10-19 16:56:10 +0800

    Factorial Trailing Zeroes 递归版.

---
+++ src/main/java/leetcode/easy/FactorialTrailingZeroes.java
@@ -0,0 +1,20 @@
+package leetcode.easy;
+
+/**
+ * Created by zhangsw on 2017/10/19.
+ */
+public class FactorialTrailingZeroes
+{
+	class Solution
+	{
+		/*
+		0是2和5相乘得到的，而在1到n这个范围内，2的个数要远多于5的个数，所以这里只需计算从1到n这个范围内有多少个5就可以了
+		但是25会贡献两个5，125会贡献三个5，这些数需要特殊考虑
+		 */
+		//递归版
+		public int trailingZeroes(int n)
+		{
+			return (n < 5) ? 0 : n / 5 + trailingZeroes(n / 5);
+		}
+	}
+}
