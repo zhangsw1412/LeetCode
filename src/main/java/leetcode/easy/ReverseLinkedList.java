ea90ec0829eeecfe3799571edde773000d699e37
zhangsw <zhangsw_1412@foxmail.com>
2017-10-27 18:23:21 +0800

    Reverse Linked List.

---
+++ src/main/java/leetcode/easy/ReverseLinkedList.java
@@ -0,0 +1,42 @@
+package leetcode.easy;
+
+/**
+ * Created by zhangsw on 2017/10/27.
+ */
+public class ReverseLinkedList
+{
+	/**
+	 * Definition for singly-linked list.
+	 */
+	class ListNode
+	{
+		int val;
+		ListNode next;
+
+		ListNode(int x)
+		{
+			val = x;
+		}
+	}
+
+	class Solution
+	{
+		public ListNode reverseList(ListNode head)
+		{
+			if (head == null || head.next == null)
+			{
+				return head;
+			}
+			ListNode fake = new ListNode(0), temp;
+			fake.next = head;
+			while (head.next != null)
+			{
+				temp = head.next;
+				head.next = temp.next;
+				temp.next = fake.next;
+				fake.next = temp;
+			}
+			return fake.next;
+		}
+	}
+}
