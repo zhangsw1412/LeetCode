4d8c89f85de69d70f39667e66cd03d84d1562913
zhangsw <zhangsw_1412@foxmail.com>
2017-10-27 18:39:14 +0800

    Reverse Linked List递归版.

--- src/main/java/leetcode/easy/ReverseLinkedList.java
+++ src/main/java/leetcode/easy/ReverseLinkedList.java
@@ -21,6 +21,19 @@ public class ReverseLinkedList
 
 	class Solution
 	{
+		//迭代方式
+		public ListNode reverseList(ListNode head)
+		{
+			if (head == null || head.next == null)
+			{
+				return head;
+			}
+			ListNode newHead = reverseList(head.next);
+			head.next.next = head;
+			head.next = null;
+			return newHead;
+		}
+		/*
 		public ListNode reverseList(ListNode head)
 		{
 			if (head == null || head.next == null)
@@ -38,5 +51,6 @@ public class ReverseLinkedList
 			}
 			return fake.next;
 		}
+		*/
 	}
 }
