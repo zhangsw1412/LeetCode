19eae9f8e448ec8f91898ee45c48a58011f020af
zhangsw <zhangsw_1412@foxmail.com>
2017-11-04 00:30:19 +0800

    Move Zeroes.简洁版.

--- src/main/java/leetcode/easy/MoveZeroes.java
+++ src/main/java/leetcode/easy/MoveZeroes.java
@@ -7,6 +7,28 @@ public class MoveZeroes
 {
 	class Solution
 	{
+		//简洁的写法
+		public void moveZeroes(int[] nums)
+		{
+			if (nums == null || nums.length == 0)
+			{
+				return;
+			}
+			int insertPos = 0;
+			for (int num : nums)
+			{
+				if (num != 0)
+				{
+					nums[insertPos++] = num;
+				}
+			}
+			while (insertPos < nums.length)
+			{
+				nums[insertPos++] = 0;
+			}
+		}
+		//自己完成的版本
+		/*
 		public void moveZeroes(int[] a)
 		{
 			int zeroCount = 0, pos = 0;
@@ -27,5 +49,6 @@ public class MoveZeroes
 				a[pos - zeroCount - 1] = 0;
 			}
 		}
+		*/
 	}
 }
