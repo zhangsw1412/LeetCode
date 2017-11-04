da1c4999a30e55440ce7b034d7e9733f40f5a3bd
zhangsw <zhangsw_1412@foxmail.com>
2017-11-05 00:23:52 +0800

    Nim Game.

---
+++ src/main/java/leetcode/easy/NimGame.java
@@ -0,0 +1,15 @@
+package leetcode.easy;
+
+/**
+ * Created by zhangsw on 2017/11/5.
+ */
+public class NimGame
+{
+	class Solution
+	{
+		public boolean canWinNim(int n)
+		{
+			return n % 4 != 0;
+		}
+	}
+}
