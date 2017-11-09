858a717c1a661157d0ec16f2922b02a9fcf53cc7
zhangsw <zhangsw_1412@foxmail.com>
2017-11-09 21:20:30 +0800

    Ransom Note简化版.

--- src/main/java/leetcode/easy/RansomNote.java
+++ src/main/java/leetcode/easy/RansomNote.java
@@ -37,5 +37,25 @@ public class RansomNote
 			}
 			return true;
 		}
+
+		//上面的解法可以用数组简化
+		/*
+		public boolean canConstruct(String ransomNote, String magazine)
+		{
+			int[] arr = new int[26];
+			for (int i = 0; i < magazine.length(); i++)
+			{
+				arr[magazine.charAt(i) - 'a']++;
+			}
+			for (int i = 0; i < ransomNote.length(); i++)
+			{
+				if (--arr[ransomNote.charAt(i) - 'a'] < 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		*/
 	}
 }
