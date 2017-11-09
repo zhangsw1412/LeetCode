9b9ed1d77b1128319503e172a5827258af034955
zhangsw <zhangsw_1412@foxmail.com>
2017-11-09 21:28:34 +0800

    Ransom Note添加关于java8方法的描述.

--- src/main/java/leetcode/easy/RansomNote.java
+++ src/main/java/leetcode/easy/RansomNote.java
@@ -10,6 +10,11 @@ public class RansomNote
 {
 	class Solution
 	{
+		/*
+		在下面这个解法中是java7的常规写法，
+		但是java8中Map添加了getOrDefault和putIfAbsent两个方法
+		使用这两个方法会让代码简化很多
+		 */
 		public boolean canConstruct(String ransomNote, String magazine)
 		{
 			Map<Character, Integer> store = new HashMap<>();
