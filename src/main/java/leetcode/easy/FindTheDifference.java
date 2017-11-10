933508bef46644ded34e9d09fc63e61a407e493f
zhangsw <zhangsw_1412@foxmail.com>
2017-11-10 17:04:41 +0800

    Find the Difference.

---
+++ src/main/java/leetcode/easy/FindTheDifference.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * Created by zhangsw on 2017/11/10.
+ */
+public class FindTheDifference
+{
+	class Solution
+	{
+		public char findTheDifference(String s, String t)
+		{
+			char[] char_s = s.toCharArray();
+			char[] char_t = t.toCharArray();
+			Arrays.sort(char_s);
+			Arrays.sort(char_t);
+			for (int i = 0; i < char_s.length; ++i)
+			{
+				if (char_s[i] != char_t[i])
+				{
+					return char_t[i];
+				}
+			}
+			return char_t[char_t.length - 1];
+		}
+	}
+}
