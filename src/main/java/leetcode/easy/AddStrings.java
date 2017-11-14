0c0cae4c2c37bd07154c5abfc74dba34b46bd523
zhangsw <zhangsw_1412@foxmail.com>
2017-11-14 17:16:21 +0800

    Add Strings改进一下.

--- src/main/java/leetcode/easy/AddStrings.java
+++ src/main/java/leetcode/easy/AddStrings.java
@@ -12,7 +12,7 @@ public class AddStrings
 			char[] a = num1.toCharArray(), b = num2.toCharArray();
 			int _a, _b, temp, carry = 0;
 			StringBuilder stringBuilder = new StringBuilder();
-			for (int i = a.length - 1, j = b.length - 1; (i >= 0 || j >= 0); --i, --j)
+			for (int i = a.length - 1, j = b.length - 1; (i >= 0 || j >= 0 || carry == 1); --i, --j)
 			{
 				_a = i >= 0 ? a[i] - '0' : 0;
 				_b = j >= 0 ? b[j] - '0' : 0;
@@ -20,7 +20,6 @@ public class AddStrings
 				carry = temp > 9 ? 1 : 0;
 				stringBuilder.append(temp % 10);
 			}
-			stringBuilder.append((carry == 1) ? 1 : "");
 			return stringBuilder.reverse().toString();
 		}
 	}
