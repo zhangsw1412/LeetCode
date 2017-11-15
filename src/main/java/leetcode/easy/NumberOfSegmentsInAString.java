12a1de499270fb9d3ac3b0941676ed42b685decb
zhangsw <zhangsw_1412@foxmail.com>
2017-11-15 15:01:22 +0800

    Number of Segments in a String.

--- src/main/java/leetcode/easy/NumberOfSegmentsInAString.java
+++ src/main/java/leetcode/easy/NumberOfSegmentsInAString.java
@@ -7,10 +7,17 @@ public class NumberOfSegmentsInAString
 {
 	class Solution
 	{
+		//原始的版本
 		public int countSegments(String s)
 		{
 			s = s.replaceAll("\\s+", " ");
 			return s.trim().length() == 0 ? 0 : s.trim().split(" ").length;
+		}
+
+		//上面的写法可以改进一下
+		public int countSegments(String s)
+		{
+			return s.trim().length() == 0 ? 0 : s.trim().split("\\s+").length;
 		}
 	}
 }
