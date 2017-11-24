dce3afe65fdc2caf4de11b70787bb77863b772b0
zhangsw <zhangsw_1412@foxmail.com>
2017-11-24 15:00:42 +0800

    Base 7.

---
+++ src/main/java/leetcode/easy/Base7.java
@@ -0,0 +1,15 @@
+package leetcode.easy;
+
+/**
+ * Created by zhangsw on 2017/11/24.
+ */
+public class Base7
+{
+	class Solution
+	{
+		public String convertToBase7(int n)
+		{
+			return Integer.toString(n, 7);
+		}
+	}
+}
