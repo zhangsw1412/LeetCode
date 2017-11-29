f1d564ae3408118badbaae7aca07f211d067d100
zhangsw <zhangsw_1412@foxmail.com>
2017-11-29 10:06:32 +0800

    Binary Tree Tilt 后序遍历解法.

--- src/main/java/leetcode/easy/BinaryTreeTilt.java
+++ src/main/java/leetcode/easy/BinaryTreeTilt.java
@@ -48,5 +48,31 @@ public class BinaryTreeTilt
 			calTilts(node.left);
 			calTilts(node.right);
 		}
+
+		//后序遍历解法
+		/*
+		int result = 0;
+
+		public int findTilt(TreeNode root)
+		{
+			postOrder(root);
+			return result;
+		}
+
+		private int postOrder(TreeNode root)
+		{
+			if (root == null)
+			{
+				return 0;
+			}
+
+			int left = postOrder(root.left);
+			int right = postOrder(root.right);
+
+			result += Math.abs(left - right);
+
+			return left + right + root.val;
+		}
+		*/
 	}
 }
