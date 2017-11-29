ed389faeb4743ec01704b5cbc8ba8a4aa6dae5b9
zhangsw <zhangsw_1412@foxmail.com>
2017-11-29 09:20:30 +0800

    Student Attendance Record I简洁版.

--- src/main/java/leetcode/easy/StudentAttendanceRecord_I.java
+++ src/main/java/leetcode/easy/StudentAttendanceRecord_I.java
@@ -12,6 +12,18 @@ public class StudentAttendanceRecord_I
 			return s.split("A").length < 3 && s.indexOf("AA") == -1 && s.indexOf("LLL") == -1;
 		}
 
+		//上面写法的简洁版
+		/*
+		public boolean checkRecord(String s)
+		{
+			if (s.indexOf("A") != s.lastIndexOf("A") || s.contains("LLL"))
+			{
+				return false;
+			}
+			return true;
+		}
+		*/
+
 		//正则表达式版
 		/*
 		public boolean checkRecord(String s)
