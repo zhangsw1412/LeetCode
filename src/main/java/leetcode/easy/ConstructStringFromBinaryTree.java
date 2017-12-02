e44b80da51dbd9e52567162b263b3b16b0d50974
zhangsw <zhangsw1412.cpp@gmail.com>
2017-12-02 10:55:25 +0800

    Construct String From Binary Tree一行版.

--- src/main/java/leetcode/easy/ConstructStringFromBinaryTree.java
+++ src/main/java/leetcode/easy/ConstructStringFromBinaryTree.java
@@ -49,6 +49,16 @@ public class ConstructStringFromBinaryTree
 			return result + "(" + left + ")" + "(" + right + ")";
 		}
 
+		//一行版
+		/*
+		public String tree2str(TreeNode t)
+		{
+			return t == null ? "" : t.val + (t.left != null ? "(" + tree2str(
+					t.left) + ")" : t.right != null ? "()" : "") + (t.right != null ? "(" + tree2str(
+					t.right) + ")" : "");
+		}
+		*/
+
 		//自己的版本
 		/*
 		public String tree2str(TreeNode root)
