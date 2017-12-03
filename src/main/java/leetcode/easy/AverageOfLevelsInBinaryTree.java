3a4e575f1c0605712107f9380a0e68c65cd2a24f
zhangsw <zhangsw_1412@foxmail.com>
2017-12-03 15:42:44 +0800

    Average of Levels in Binary Tree广度优先遍历.

--- src/main/java/leetcode/easy/AverageOfLevelsInBinaryTree.java
+++ src/main/java/leetcode/easy/AverageOfLevelsInBinaryTree.java
@@ -1,7 +1,9 @@
 package leetcode.easy;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by zhangsw on 2017/12/3.
@@ -53,5 +55,40 @@ public class AverageOfLevelsInBinaryTree
 			helper(node.left, level + 1, sum, count);
 			helper(node.right, level + 1, sum, count);
 		}
+
+		//广度优先遍历
+		/*
+		public List<Double> averageOfLevels(TreeNode root)
+		{
+			List<Double> result = new ArrayList<>();
+			Queue<TreeNode> q = new LinkedList<>();
+
+			if (root == null)
+			{
+				return result;
+			}
+			q.add(root);
+			while (!q.isEmpty())
+			{
+				int n = q.size();
+				double sum = 0.0;
+				for (int i = 0; i < n; i++)
+				{
+					TreeNode node = q.poll();
+					sum += node.val;
+					if (node.left != null)
+					{
+						q.offer(node.left);
+					}
+					if (node.right != null)
+					{
+						q.offer(node.right);
+					}
+				}
+				result.add(sum / n);
+			}
+			return result;
+		}
+		*/
 	}
 }
