209177bca621594376e24a63915c2994a6232fa0
zhangsw <zhangsw_1412@foxmail.com>
2017-12-07 22:44:02 +0800

    Valid Palindrome II.

---
+++ src/main/java/leetcode/easy/ValidPalindrome_II.java
@@ -0,0 +1,35 @@
+package leetcode.easy;
+
+/**
+ * Created by zhangsw on 2017/12/7.
+ */
+public class ValidPalindrome_II
+{
+	class Solution
+	{
+		public boolean validPalindrome(String s)
+		{
+			int l = -1, r = s.length();
+			while (++l < --r)
+			{
+				if (s.charAt(l) != s.charAt(r))
+				{
+					return isPalindromic(s, l, r + 1) || isPalindromic(s, l - 1, r);
+				}
+			}
+			return true;
+		}
+
+		public boolean isPalindromic(String s, int l, int r)
+		{
+			while (++l < --r)
+			{
+				if (s.charAt(l) != s.charAt(r))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
