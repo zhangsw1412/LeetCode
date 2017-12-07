86fd0669757fe59aee1328dde19438682a798ac7
zhangsw <zhangsw_1412@foxmail.com>
2017-12-07 22:05:20 +0800

    Longest Continuous Increasing Subsequence简洁版.

--- src/main/java/leetcode/easy/LongestContinuousIncreasingSubsequence.java
+++ src/main/java/leetcode/easy/LongestContinuousIncreasingSubsequence.java
@@ -29,5 +29,22 @@ public class LongestContinuousIncreasingSubsequence
 			}
 			return length;
 		}
+
+		//简洁版
+		/*
+		public int findLengthOfLCIS(int[] a)
+		{
+			int mx = 0, len = 0;
+			for (int i = 0; i < a.length; i++)
+			{
+				if (i == 0 || a[i] <= a[i - 1])
+				{
+					len = 0;
+				}
+				mx = Math.max(mx, ++len);
+			}
+			return mx;
+		}
+		*/
 	}
 }
