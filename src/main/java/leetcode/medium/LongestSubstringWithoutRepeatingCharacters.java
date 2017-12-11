d1df2ba804e79ff15dd63f00ce84c4333cbb81df
zhangsw <zhangsw1412.cpp@gmail.com>
2017-12-11 20:07:29 +0800

    Longest Substring Without Repeating Characters map版.

--- src/main/java/leetcode/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/leetcode/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,6 @@
 package leetcode.medium;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -58,6 +59,29 @@ public class LongestSubstringWithoutRepeatingCharacters
 				}
 			}
 
+			return max;
+		}
+		*/
+
+		//map版
+		/*
+		public int lengthOfLongestSubstring(String s)
+		{
+			if (s.length() == 0)
+			{
+				return 0;
+			}
+			HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+			int max = 0;
+			for (int i = 0, j = 0; i < s.length(); ++i)
+			{
+				if (map.containsKey(s.charAt(i)))
+				{
+					j = Math.max(j, map.get(s.charAt(i)) + 1);
+				}
+				map.put(s.charAt(i), i);
+				max = Math.max(max, i - j + 1);
+			}
 			return max;
 		}
 		*/
