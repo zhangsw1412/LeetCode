147cc08865b46a26f6095098b3f7f7a56063b6ed
zhangsw <zhangsw_1412@foxmail.com>
2017-12-12 10:33:27 +0800

    ZigZag Conversion周期法.

--- src/main/java/leetcode/medium/ZigZagConversion.java
+++ src/main/java/leetcode/medium/ZigZagConversion.java
@@ -44,5 +44,37 @@ public class ZigZagConversion
 			}
 			return ret;
 		}
+
+		//周期法
+		/*
+		public String convert(String s, int nRows)
+		{
+			int len = s.length();
+			if (len == 0 || nRows < 2)
+			{
+				return s;
+			}
+
+			String ret = "";
+			int lag = 2 * nRows - 2; //循环周期
+			for (int i = 0; i < nRows; i++)
+			{
+				for (int j = i; j < len; j += lag)
+				{
+					ret += s.charAt(j);
+					//非首行和末行时还要加一个
+					if (i > 0 && i < nRows - 1)
+					{
+						int t = j + lag - 2 * i;
+						if (t < len)
+						{
+							ret += s.charAt(t);
+						}
+					}
+				}
+			}
+			return ret;
+		}
+		*/
 	}
 }
