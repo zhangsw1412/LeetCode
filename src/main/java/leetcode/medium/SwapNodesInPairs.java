9f074777fb9f8946c63cbace55f0ef7e860880e8
zhangsw <zhangsw1412.cpp@gmail.com>
2017-12-14 20:42:28 +0800

    Swap Nodes in Pairs递归版.

--- src/main/java/leetcode/medium/SwapNodesInPairs.java
+++ src/main/java/leetcode/medium/SwapNodesInPairs.java
@@ -42,5 +42,20 @@ public class SwapNodesInPairs
 			}
 			return fake.next;
 		}
+
+		//递归版
+		/*
+		public ListNode swapPairs(ListNode head)
+		{
+			if ((head == null) || (head.next == null))
+			{
+				return head;
+			}
+			ListNode n = head.next;
+			head.next = swapPairs(head.next.next);
+			n.next = head;
+			return n;
+		}
+		*/
 	}
 }
