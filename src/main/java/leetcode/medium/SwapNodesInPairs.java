d608844a07a5362a2e445d13bde37907c3aa5573
zhangsw <zhangsw1412.cpp@gmail.com>
2017-12-14 20:41:29 +0800

    Swap Nodes in Pairs.

---
+++ src/main/java/leetcode/medium/SwapNodesInPairs.java
@@ -0,0 +1,46 @@
+package leetcode.medium;
+
+/**
+ * Created by zhangsw on 2017/12/14.
+ */
+public class SwapNodesInPairs
+{
+	/**
+	 * Definition for singly-linked list.
+	 */
+	class ListNode
+	{
+		int val;
+		ListNode next;
+
+		ListNode(int x)
+		{
+			val = x;
+		}
+	}
+
+	class Solution
+	{
+		public ListNode swapPairs(ListNode head)
+		{
+			ListNode fake = new ListNode(0), left, right, l, r;
+			fake.next = head;
+			left = fake;
+			while (left != null)
+			{
+				l = left.next == null ? null : left.next;
+				r = l == null ? l : l.next;
+				right = r == null ? r : r.next;
+				if (l == null || r == null)
+				{
+					break;
+				}
+				left.next = r;
+				r.next = l;
+				l.next = right;
+				left = left.next.next;
+			}
+			return fake.next;
+		}
+	}
+}
