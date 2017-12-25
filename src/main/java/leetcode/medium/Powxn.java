4c5b7689f129b5dc5af58b0a062218dc7c0ec5e9
zhangsw <zhangsw1412.cpp@gmail.com>
2017-12-25 15:55:11 +0800

    Pow(x,n)非作弊版.

--- src/main/java/leetcode/medium/Powxn.java
+++ src/main/java/leetcode/medium/Powxn.java
@@ -7,10 +7,25 @@ public class Powxn
 {
 	class Solution
 	{
+		public double pow(double x, int n)
+		{
+			if (n == 0)
+			{
+				return 1;
+			}
+			if (n < 0)
+			{
+				n = -n;
+				x = 1 / x;
+			}
+			return (n % 2 == 0) ? pow(x * x, n / 2) : x * pow(x * x, n / 2);
+		}
 		//这个写法有点作弊...
+		/*
 		public double myPow(double x, int n)
 		{
 			return Math.pow(x, n);
 		}
+		*/
 	}
 }
