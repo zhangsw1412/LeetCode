23b0300b93adea8ac18fa3fef27bb135c6b7bd0c
zhangsw <zhangsw1412.cpp@gmail.com>
2017-12-25 15:51:59 +0800

    Pow(x,n).

---
+++ src/main/java/leetcode/medium/Powxn.java
@@ -0,0 +1,16 @@
+package leetcode.medium;
+
+/**
+ * Created by zhangsw on 2017/12/25.
+ */
+public class Powxn
+{
+	class Solution
+	{
+		//这个写法有点作弊...
+		public double myPow(double x, int n)
+		{
+			return Math.pow(x, n);
+		}
+	}
+}
