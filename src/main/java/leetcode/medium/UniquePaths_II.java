8e8a514aa82b6a8ff1644c38ea695bbc556e9c00
zhangsw <zhangsw1412.cpp@gmail.com>
2017-12-26 15:55:16 +0800

    Unique Paths II简洁版.

--- src/main/java/leetcode/medium/UniquePaths_II.java
+++ src/main/java/leetcode/medium/UniquePaths_II.java
@@ -7,6 +7,29 @@ public class UniquePaths_II
 {
 	class Solution
 	{
+		//简洁版
+		public int uniquePathsWithObstacles(int[][] obstacleGrid)
+		{
+			int width = obstacleGrid[0].length;
+			int[] dp = new int[width];
+			dp[0] = 1;
+			for (int[] row : obstacleGrid)
+			{
+				for (int j = 0; j < width; j++)
+				{
+					if (row[j] == 1)
+					{
+						dp[j] = 0;
+					}
+					else if (j > 0)
+					{
+						dp[j] += dp[j - 1];
+					}
+				}
+			}
+			return dp[width - 1];
+		}
+		/*
 		public int uniquePathsWithObstacles(int[][] a)
 		{
 			if (a == null)
@@ -51,5 +74,6 @@ public class UniquePaths_II
 			}
 			return a[a.length - 1][a[0].length - 1] == -1 ? 0 : a[a.length - 1][a[0].length - 1];
 		}
+		*/
 	}
 }
