39496991636b999387c76f14216f72c3177f92d4
zhangsw <zhangsw1412.cpp@gmail.com>
2017-12-26 15:02:56 +0800

    Rotate List直接的解法.

--- src/main/java/leetcode/medium/RotateList.java
+++ src/main/java/leetcode/medium/RotateList.java
@@ -43,5 +43,37 @@ public class RotateList
 			}
 			return fake.next;
 		}
+
+		//另一种比较直接的思路
+		/*
+		public ListNode rotateRight(ListNode head, int k)
+		{
+			if (head == null)
+			{
+				return head;
+			}
+
+			ListNode copyHead = head;
+
+			int len = 1;
+			while (copyHead.next != null)
+			{
+				copyHead = copyHead.next;
+				len++;
+			}
+
+			copyHead.next = head;
+
+			for (int i = len - k % len; i > 1; i--)
+			{
+				head = head.next;
+			}
+
+			copyHead = head.next;
+			head.next = null;
+
+			return copyHead;
+		}
+		*/
 	}
 }
