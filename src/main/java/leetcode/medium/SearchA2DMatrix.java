633040dd3ee9e62e377ce45daa76bc6ed82c3dcb
zhangsw <zhangsw_1412@foxmail.com>
2017-12-28 15:16:25 +0800

    Search a 2D Matrix直接版.

--- src/main/java/leetcode/medium/SearchA2DMatrix.java
+++ src/main/java/leetcode/medium/SearchA2DMatrix.java
@@ -9,6 +9,29 @@ import java.util.List;
  */
 public class SearchA2DMatrix
 {
+	//直接版
+	public boolean searchMatrix(int[][] matrix, int target)
+	{
+		int i = 0, j = matrix[0].length - 1;
+		while (i < matrix.length && j >= 0)
+		{
+			if (matrix[i][j] == target)
+			{
+				return true;
+			}
+			else if (matrix[i][j] > target)
+			{
+				j--;
+			}
+			else
+			{
+				i++;
+			}
+		}
+
+		return false;
+	}
+	/*
 	public boolean searchMatrix(int[][] matrix, int target)
 	{
 		if (matrix == null || matrix.length == 0)
@@ -26,4 +49,5 @@ public class SearchA2DMatrix
 		}
 		return Arrays.binarySearch(list, target) >= 0;
 	}
+	*/
 }
