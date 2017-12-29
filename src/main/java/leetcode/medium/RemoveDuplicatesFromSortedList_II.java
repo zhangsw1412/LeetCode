b4e75a1e0db1a8ac912e8d29ed5cfc6bd8b937ff
zhangsw <zhangsw_1412@foxmail.com>
2017-12-30 00:03:41 +0800

    Remove Duplicates from Sorted List II简洁版.

--- src/main/java/leetcode/medium/RemoveDuplicatesFromSortedList_II.java
+++ src/main/java/leetcode/medium/RemoveDuplicatesFromSortedList_II.java
@@ -21,6 +21,37 @@ public class RemoveDuplicatesFromSortedList_II
 
 	class Solution
 	{
+		//简洁版
+		public ListNode deleteDuplicates(ListNode head)
+		{
+			if (head == null)
+			{
+				return null;
+			}
+			ListNode FakeHead = new ListNode(0);
+			FakeHead.next = head;
+			ListNode pre = FakeHead;
+			ListNode cur = head;
+			while (cur != null)
+			{
+				while (cur.next != null && cur.val == cur.next.val)
+				{
+					cur = cur.next;
+				}
+				if (pre.next == cur)
+				{
+					pre = pre.next;
+				}
+				else
+				{
+					pre.next = cur.next;
+				}
+				cur = cur.next;
+			}
+			return FakeHead.next;
+		}
+
+		/*
 		public ListNode deleteDuplicates(ListNode head)
 		{
 			if (head == null)
@@ -52,5 +83,6 @@ public class RemoveDuplicatesFromSortedList_II
 			}
 			return fake.next;
 		}
+		*/
 	}
 }
