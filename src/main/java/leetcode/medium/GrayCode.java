0f13af573e73b2947d6e0092aeaa888cb8f44407
zhangsw <zhangsw_1412@foxmail.com>
2017-12-31 09:52:05 +0800

    Gray Code换代法.

--- src/main/java/leetcode/medium/GrayCode.java
+++ src/main/java/leetcode/medium/GrayCode.java
@@ -1,6 +1,6 @@
 package leetcode.medium;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,6 +10,22 @@ public class GrayCode
 {
 	class Solution
 	{
+		//迭代法
+		public List<Integer> grayCode(int n)
+		{
+			List<Integer> rs = new ArrayList<Integer>();
+			rs.add(0);
+			for (int i = 0; i < n; i++)
+			{
+				int size = rs.size();
+				for (int k = size - 1; k >= 0; k--)
+				{
+					rs.add(rs.get(k) | 1 << i);
+				}
+			}
+			return rs;
+		}
+		/*
 		public List<Integer> grayCode(int n)
 		{
 			//看不懂...
@@ -20,5 +36,6 @@ public class GrayCode
 			}
 			return result;
 		}
+		*/
 	}
 }
