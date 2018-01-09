418b0386f359d11a2f29284bae7b7cc514e2d7fd
zhangsw <zhangsw_1412@foxmail.com>
2018-01-09 11:06:19 +0800

    Binary Tree Zigzag Level Order Traversal简洁版.

--- src/main/java/leetcode/medium/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/main/java/leetcode/medium/BinaryTreeZigzagLevelOrderTraversal.java
@@ -27,6 +27,48 @@ public class BinaryTreeZigzagLevelOrderTraversal
 
 	class Solution
 	{
+		//简洁版
+		public List<List<Integer>> zigzagLevelOrder(TreeNode root)
+		{
+			List<List<Integer>> res = new ArrayList<>();
+			if (root == null)
+			{
+				return res;
+			}
+			Queue<TreeNode> queue = new LinkedList<>();
+			queue.add(root);
+			boolean zigzag = false;
+			while (!queue.isEmpty())
+			{
+				List<Integer> level = new ArrayList<>();
+				int cnt = queue.size();
+				for (int i = 0; i < cnt; i++)
+				{
+					TreeNode node = queue.poll();
+					if (zigzag)
+					{
+						level.add(0, node.val);
+					}
+					else
+					{
+						level.add(node.val);
+					}
+					if (node.left != null)
+					{
+						queue.add(node.left);
+					}
+					if (node.right != null)
+					{
+						queue.add(node.right);
+					}
+				}
+				res.add(level);
+				zigzag = !zigzag;
+			}
+			return res;
+		}
+
+		/*
 		public List<List<Integer>> zigzagLevelOrder(TreeNode root)
 		{
 			List<List<Integer>> list = new ArrayList<>();
@@ -63,5 +105,6 @@ public class BinaryTreeZigzagLevelOrderTraversal
 			}
 			return list;
 		}
+		*/
 	}
 }
