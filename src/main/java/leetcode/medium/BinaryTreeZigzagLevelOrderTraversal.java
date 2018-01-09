916cb3a27664e49e79e5462a379535e64b63e292
zhangsw <zhangsw_1412@foxmail.com>
2018-01-09 11:01:18 +0800

    Binary Tree Zigzag Level Order Traversal.

---
+++ src/main/java/leetcode/medium/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,67 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by zhangsw on 2018/1/9.
+ */
+public class BinaryTreeZigzagLevelOrderTraversal
+{
+	/**
+	 * Definition for a binary tree node.
+	 */
+	class TreeNode
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x)
+		{
+			val = x;
+		}
+	}
+
+	class Solution
+	{
+		public List<List<Integer>> zigzagLevelOrder(TreeNode root)
+		{
+			List<List<Integer>> list = new ArrayList<>();
+			if (root == null)
+			{
+				return list;
+			}
+			LinkedList<TreeNode> queue = new LinkedList<>();
+			queue.offer(root);
+			boolean direction = true;
+			while (!queue.isEmpty())
+			{
+				int length = queue.size();
+				TreeNode node;
+				for (int i = 0; i < length; i++)
+				{
+					node = queue.get(i);
+					if (node.left != null)
+					{
+						queue.offer(node.left);
+					}
+					if (node.right != null)
+					{
+						queue.offer(node.right);
+					}
+				}
+				List<Integer> temp = new ArrayList<>();
+				for (int i = direction ? 0 : length - 1; direction ? i < length : i >= 0; i = direction ? i + 1 : i - 1)
+				{
+					temp.add(direction ? queue.remove(0).val : queue.remove(i).val);
+				}
+				list.add(temp);
+				direction = !direction;
+			}
+			return list;
+		}
+	}
+}
