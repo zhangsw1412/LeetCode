ad83d6ff4a489393228ebb0ac4b57016fea629b5
zhangsw <zhangsw_1412@foxmail.com>
2018-01-12 10:23:44 +0800

    Flatten Binary Tree to Linked List迭代版.

--- src/main/java/leetcode/medium/FlattenBinaryTreeToLinkedList.java
+++ src/main/java/leetcode/medium/FlattenBinaryTreeToLinkedList.java
@@ -39,5 +39,28 @@ public class FlattenBinaryTreeToLinkedList
 			}
 			root.right = right;
 		}
+
+		//迭代版
+		/*
+		public void flatten(TreeNode root)
+		{
+			TreeNode current = root;
+			while (current != null)
+			{
+				if (current.left != null)
+				{
+					TreeNode temp = current.left;
+					while (temp.right != null)
+					{
+						temp = temp.right;
+					}
+					temp.right = current.right;
+					current.right = current.left;
+					current.left = null;
+				}
+				current = current.right;
+			}
+		}
+		*/
 	}
 }
