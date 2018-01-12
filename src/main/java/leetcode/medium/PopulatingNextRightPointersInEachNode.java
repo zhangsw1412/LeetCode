d2185d2fe972e210a764b721b66a0e254e716dec
zhangsw <zhangsw_1412@foxmail.com>
2018-01-12 10:42:13 +0800

    Populating Next Right Pointers in Each Node递归版.

--- src/main/java/leetcode/medium/PopulatingNextRightPointersInEachNode.java
+++ src/main/java/leetcode/medium/PopulatingNextRightPointersInEachNode.java
@@ -49,5 +49,26 @@ public class PopulatingNextRightPointersInEachNode
 				}
 			}
 		}
+
+		//递归版
+		/*
+		public void connect(TreeLinkNode root)
+		{
+			if (root == null)
+			{
+				return;
+			}
+			if (root.left != null)
+			{
+				root.left.next = root.right;
+				if (root.next != null)
+				{
+					root.right.next = root.next.left;
+				}
+			}
+			connect(root.left);
+			connect(root.right);
+		}
+		*/
 	}
 }
