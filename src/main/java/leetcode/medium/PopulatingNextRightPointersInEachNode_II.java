83321c0a40411aa2db9144ce2472a4ef4df5e30c
zhangsw <zhangsw1412.cpp@gmail.com>
2018-01-17 19:59:53 +0800

    Populating Next Right Pointers in Each Node II.

---
+++ src/main/java/leetcode/medium/PopulatingNextRightPointersInEachNode_II.java
@@ -0,0 +1,53 @@
+package leetcode.medium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by zhangsw on 2018/1/17.
+ */
+public class PopulatingNextRightPointersInEachNode_II
+{
+	/**
+	 * Definition for a binary tree with next pointer.
+	 */
+	class TreeLinkNode
+	{
+		int val;
+		TreeLinkNode left, right, next;
+
+		TreeLinkNode(int x)
+		{
+			val = x;
+		}
+	}
+
+	class Solution
+	{
+		public void connect(TreeLinkNode root)
+		{
+			if (root == null)
+			{
+				return;
+			}
+			Queue<TreeLinkNode> queue = new LinkedList<>();
+			queue.offer(root);
+			while (!queue.isEmpty())
+			{
+				int length = queue.size();
+				for (int i = 0; i < length; i++)
+				{
+					if (queue.peek().left != null)
+					{
+						queue.offer(queue.peek().left);
+					}
+					if (queue.peek().right != null)
+					{
+						queue.offer(queue.peek().right);
+					}
+					queue.poll().next = i == length - 1 ? null : queue.peek();
+				}
+			}
+		}
+	}
+}
