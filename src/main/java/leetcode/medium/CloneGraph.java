36df8c69046782bb7eaa0b18c7740064b52a7242
zhangsw <zhangsw1412.cpp@gmail.com>
2018-01-23 09:28:46 +0800

    Clone Graph.广度优先遍历.

--- src/main/java/leetcode/medium/CloneGraph.java
+++ src/main/java/leetcode/medium/CloneGraph.java
@@ -2,6 +2,7 @@ package leetcode.medium;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -53,5 +54,40 @@ public class CloneGraph
 			}
 			return clone;
 		}
+
+		//广度优先遍历
+		/*
+		public UndirectedGraphNode cloneGraph(UndirectedGraphNode node)
+		{
+			if (node == null)
+			{
+				return null;
+			}
+
+			UndirectedGraphNode newNode = new UndirectedGraphNode(node.label); //new node for return
+			HashMap<Integer, UndirectedGraphNode> map = new HashMap(); //store visited nodes
+
+			map.put(newNode.label, newNode); //add first node to HashMap
+
+			LinkedList<UndirectedGraphNode> queue = new LinkedList(); //to store **original** nodes need to be visited
+			queue.add(node); //add first **original** node to queue
+
+			while (!queue.isEmpty())
+			{ //if more nodes need to be visited
+				UndirectedGraphNode n = queue.pop(); //search first node in the queue
+				for (UndirectedGraphNode neighbor : n.neighbors)
+				{
+					if (!map.containsKey(neighbor.label))
+					{ //add to map and queue if this node hasn't been searched before
+						map.put(neighbor.label, new UndirectedGraphNode(neighbor.label));
+						queue.add(neighbor);
+					}
+					map.get(n.label).neighbors.add(map.get(neighbor.label)); //add neighbor to new created nodes
+				}
+			}
+
+			return newNode;
+		}
+		*/
 	}
 }
