e593d3c94c36c29167cb8c66995990b290f9e06e
zhangsw <zhangsw1412.cpp@gmail.com>
2018-03-19 09:42:10 +0800

    Binary Tree Right Side View.

---
+++ src/main/java/leetcode/medium/BinaryTreeRightSideView.java
@@ -0,0 +1,53 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zhangsw on 2018/3/19.
+ */
+public class BinaryTreeRightSideView
+{
+	/**
+	 * Definition for a binary tree node.
+	 */
+	class TreeNode
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x)
+		{
+			val = x;
+		}
+	}
+
+	class Solution
+	{
+		public List<Integer> rightSideView(TreeNode root)
+		{
+			List<Integer> list = new ArrayList<>();
+			inOrder(root, 0, list);
+			return list;
+		}
+
+		private void inOrder(TreeNode node, int height, List<Integer> list)
+		{
+
+			if (node != null)
+			{
+				if (list.size() == height)
+				{
+					list.add(node.val);
+				}
+				else
+				{
+					list.set(height, node.val);
+				}
+				inOrder(node.left, height + 1, list);
+				inOrder(node.right, height + 1, list);
+			}
+		}
+	}
+}
