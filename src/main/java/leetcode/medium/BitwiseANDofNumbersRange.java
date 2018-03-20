d5cd228fe8b6839c0567d72bc9f5b819293219dd
zhangsw <zhangsw1412.cpp@gmail.com>
2018-03-20 20:55:28 +0800

    Bitwise AND of Numbers Range去1版.

--- src/main/java/leetcode/medium/BitwiseANDofNumbersRange.java
+++ src/main/java/leetcode/medium/BitwiseANDofNumbersRange.java
@@ -52,5 +52,17 @@ public class BitwiseANDofNumbersRange
 			return m < n ? rangeBitwiseAnd(m >> 1, n >> 1) << 1 : m;
 		}
 		*/
+
+		/*
+		如果m小于n就进行循环，n与上n-1，相当于去掉最低位的1，n就这样每次去掉最低位的1，如果小于等于m了，返回此时的n即可
+		 */
+		public int rangeBitwiseAnd(int m, int n)
+		{
+			while (m < n)
+			{
+				n &= n - 1;
+			}
+			return n;
+		}
 	}
 }
