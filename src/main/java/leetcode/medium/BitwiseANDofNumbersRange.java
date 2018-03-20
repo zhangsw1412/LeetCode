93c7b26a6ea9fd17c62e3c594762330343de86cd
zhangsw <zhangsw1412.cpp@gmail.com>
2018-03-20 20:46:10 +0800

    Bitwise AND of Numbers Range直接移位版.

--- src/main/java/leetcode/medium/BitwiseANDofNumbersRange.java
+++ src/main/java/leetcode/medium/BitwiseANDofNumbersRange.java
@@ -24,5 +24,20 @@ public class BitwiseANDofNumbersRange
 			}
 			return m & mask;
 		}
+
+		/*
+		直接平移m和n，每次向右移一位，直到m和n相等，记录下所有平移的次数i，然后再把m左移i位即为最终结果
+		 */
+		public int rangeBitwiseAnd(int m, int n)
+		{
+			int counter = 0;
+			while (m != n)
+			{
+				m >>= 1;
+				n >>= 1;
+				++counter;
+			}
+			return m << counter;
+		}
 	}
 }
