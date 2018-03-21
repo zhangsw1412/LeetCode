5cbd3c322b4c85ef398c5c64ee0b1ad1c82ad698
zhangsw <zhangsw_1412@foxmail.com>
2018-03-21 17:43:29 +0800

    Course Schedule广度优先遍历.

---
+++ src/main/java/leetcode/medium/CourseSchedule.java
@@ -0,0 +1,68 @@
+package leetcode.medium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by zhangsw on 2018/3/21.
+ */
+public class CourseSchedule
+{
+	class Solution
+	{
+		/*
+		使用BFS实现，定义二维数组graph来表示这个有向图，一维数组in来表示每个顶点的入度。
+		开始先根据输入来建立这个有向图，并将入度数组也初始化好。
+		然后定义一个queue变量，将所有入度为0的点放入队列中，然后开始遍历队列，
+		从graph里遍历其连接的点，每到达一个新节点，将其入度减一，
+		如果此时该点入度为0，则放入队列末尾。直到遍历完队列中所有的值，
+		若此时还有节点的入度不为0，则说明环存在，返回false，反之则返回true
+		 */
+		public boolean canFinish(int numCourses, int[][] prerequisites)
+		{
+			if (prerequisites == null || prerequisites.length == 0)
+			{
+				return true;
+			}
+
+			int[][] graph = new int[numCourses][numCourses];
+			int[] indegree = new int[numCourses];
+
+			for (int[] prerequisite : prerequisites)
+			{
+				if (graph[prerequisite[0]][prerequisite[1]] == 0)//防止一条边重复出现
+				{
+					++indegree[prerequisite[1]];
+				}
+				graph[prerequisite[0]][prerequisite[1]] = 1;
+			}
+
+			Queue<Integer> queue = new LinkedList<>();
+			for (int i = 0; i < indegree.length; i++)
+			{
+				if (indegree[i] == 0)
+				{
+					queue.offer(i);
+				}
+			}
+
+			int count = 0;
+			while (!queue.isEmpty())
+			{
+				++count;
+				int temp = queue.poll();
+				for (int i = 0; i < graph[temp].length; i++)
+				{
+					if (graph[temp][i] != 0)
+					{
+						if (--indegree[i] == 0)
+						{
+							queue.offer(i);
+						}
+					}
+				}
+			}
+			return count == numCourses;
+		}
+	}
+}
