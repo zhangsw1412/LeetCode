7a458cf0d178e6e0454f567eaeb55992dfeac7d0
zhangsw <zhangsw1412.cpp@gmail.com>
2018-03-22 20:35:26 +0800

    Minimum Size Subarray Sum简洁版.

--- src/main/java/leetcode/medium/MinimumSizeSubarraySum.java
+++ src/main/java/leetcode/medium/MinimumSizeSubarraySum.java
@@ -32,5 +32,23 @@ public class MinimumSizeSubarraySum
 			}
 			return result;
 		}
+
+		//简洁版
+		/*
+		public int minSubArrayLen(int s, int[] nums)
+		{
+			int sum = 0, from = 0, win = Integer.MAX_VALUE;
+			for (int i = 0; i < nums.length; i++)
+			{
+				sum += nums[i];
+				while (sum >= s)
+				{
+					win = Math.min(win, i - from + 1);
+					sum -= nums[from++];
+				}
+			}
+			return (win == Integer.MAX_VALUE) ? 0 : win;
+		}
+		*/
 	}
 }
