46a38b3febcb74f628767e8168d47ff39ceeacea
zhangsw <zhangsw_1412@foxmail.com>
2018-03-28 11:21:15 +0800

    Count Complete Tree Nodes简洁版.

--- src/main/java/leetcode/medium/CountCompleteTreeNodes.java
+++ src/main/java/leetcode/medium/CountCompleteTreeNodes.java
@@ -48,9 +48,33 @@ public class CountCompleteTreeNodes
 			}
 			if (hleft == hright)
 			{
-				return (1<<hleft) - 1;
+				return (1 << hleft) - 1;
 			}
 			return countNodes(root.left) + countNodes(root.right) + 1;
 		}
+
+		//简洁版
+		/*
+		public int countNodes(TreeNode root)
+		{
+			if (root == null)
+			{
+				return 0;
+			}
+			TreeNode left = root, right = root;
+			int height = 0;
+			while (right != null)
+			{
+				left = left.left;
+				right = right.right;
+				height++;
+			}
+			if (left == null)
+			{
+				return (1 << height) - 1;
+			}
+			return 1 + countNodes(root.left) + countNodes(root.right);
+		}
+		*/
 	}
 }
