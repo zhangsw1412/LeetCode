03a201c14559aa4c43ce96cf99a3feb63e31a401
zhangsw <zhangsw_1412@foxmail.com>
2018-03-28 11:19:08 +0800

    Count Complete Tree Nodes.

---
+++ src/main/java/leetcode/medium/CountCompleteTreeNodes.java
@@ -0,0 +1,56 @@
+package leetcode.medium;
+
+/**
+ * Created by zhangsw on 2018/3/28.
+ */
+public class CountCompleteTreeNodes
+{
+	/**
+	 * Definition for a binary tree node.
+	 */
+	class TreeNode
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x)
+		{
+			val = x;
+		}
+	}
+
+	class Solution
+	{
+		/*
+		如果从某节点一直向左的高度 = 一直向右的高度, 那么以该节点为root的子树一定是complete binary tree.
+		而 complete binary tree的节点数,可以用公式算出 2^h - 1.
+		如果高度不相等, 则递归调用 return countNode(left) + countNode(right) + 1.
+		复杂度为O(h^2)
+		 */
+		public int countNodes(TreeNode root)
+		{
+			if (root == null)
+			{
+				return 0;
+			}
+			int hleft = 0, hright = 0;
+			TreeNode left = root, right = root;
+			while (left != null)
+			{
+				++hleft;
+				left = left.left;
+			}
+			while (right != null)
+			{
+				++hright;
+				right = right.right;
+			}
+			if (hleft == hright)
+			{
+				return (1<<hleft) - 1;
+			}
+			return countNodes(root.left) + countNodes(root.right) + 1;
+		}
+	}
+}
