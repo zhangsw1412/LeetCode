1cf40baeb8fb87d67bd013e81c958958cddb15a4
zhangsw <zhangsw1412.cpp@gmail.com>
2018-03-29 09:17:36 +0800

    Basic Calculator栈版本.

---
+++ src/main/java/leetcode/medium/BasicCalculator_II.java
@@ -0,0 +1,60 @@
+package leetcode.medium;
+
+import java.util.Stack;
+
+/**
+ * Created by zhangsw on 2018/3/29.
+ */
+public class BasicCalculator_II
+{
+	class Solution
+	{
+		//栈版本
+		public int calculate(String s)
+		{
+			int len;
+			if (s == null || (len = s.length()) == 0)
+			{
+				return 0;
+			}
+			Stack<Integer> stack = new Stack<>();
+			int num = 0;
+			char sign = '+';
+			for (int i = 0; i < len; i++)
+			{
+				if (Character.isDigit(s.charAt(i)))
+				{
+					num = num * 10 + s.charAt(i) - '0';
+				}
+				if ((!Character.isDigit(s.charAt(i)) && ' ' != s.charAt(i)) || i == len - 1)
+				{
+					if (sign == '-')
+					{
+						stack.push(-num);
+					}
+					if (sign == '+')
+					{
+						stack.push(num);
+					}
+					if (sign == '*')
+					{
+						stack.push(stack.pop() * num);
+					}
+					if (sign == '/')
+					{
+						stack.push(stack.pop() / num);
+					}
+					sign = s.charAt(i);
+					num = 0;
+				}
+			}
+
+			int re = 0;
+			for (int i : stack)
+			{
+				re += i;
+			}
+			return re;
+		}
+	}
+}
