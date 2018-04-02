b6959772e308e66c182600688a013f62570d09af
zhangsw <zhangsw_1412@foxmail.com>
2018-04-02 15:07:32 +0800

    Lowest Common Ancestor of a Binary Tree优化.

--- src/main/java/leetcode/medium/LowestCommonAncestorOfABinaryTree.java
+++ src/main/java/leetcode/medium/LowestCommonAncestorOfABinaryTree.java
@@ -35,4 +35,33 @@ public class LowestCommonAncestorOfABinaryTree
 		}
 		return left == null ? right : left;
 	}
+
+	/*
+	上面的代码可以进行优化一下，在找完左子树的共同父节点时如果结果存在，且不是p或q，
+	那么不用再找右子树了，直接返回这个结果即可，同理，对找完右子树的结果做同样处理
+	 */
+	/*
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+	{
+		if (root == null || root == p || root == q)
+		{
+			return root;
+		}
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		if (left != null && left != p && left != q)
+		{
+			return left;
+		}
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+		if (right != null && right != p && right != q)
+		{
+			return right;
+		}
+		if (left != null && right != null)
+		{
+			return root;
+		}
+		return left == null ? right : left;
+	}
+	*/
 }
