985d893d43d7b3962b5aa52703b79bc029bc6207
zhangsw <zhangsw_1412@foxmail.com>
2018-04-08 22:06:43 +0800

    H-Index桶排序版.

--- src/main/java/leetcode/medium/H_Index.java
+++ src/main/java/leetcode/medium/H_Index.java
@@ -19,5 +19,35 @@ public class H_Index
 			}
 			return --i;
 		}
+
+		//桶排序版
+		/*
+		public int hIndex(int[] citations)
+		{
+			int n = citations.length;
+			int[] buckets = new int[n + 1];
+			for (int c : citations)
+			{
+				if (c >= n)
+				{
+					buckets[n]++;
+				}
+				else
+				{
+					buckets[c]++;
+				}
+			}
+			int count = 0;
+			for (int i = n; i >= 0; i--)
+			{
+				count += buckets[i];
+				if (count >= i)
+				{
+					return i;
+				}
+			}
+			return 0;
+		}
+		*/
 	}
 }
