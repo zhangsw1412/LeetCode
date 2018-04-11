3cdd34f2012d50dec9f3255ec325bfc649b352ac
zhangsw <zhangsw_1412@foxmail.com>
2018-04-11 10:46:24 +0800

    H-Index_II二分法版.

--- src/main/java/leetcode/medium/H_Index_II.java
+++ src/main/java/leetcode/medium/H_Index_II.java
@@ -16,5 +16,33 @@ public class H_Index_II
 			}
 			return --i;
 		}
+
+		//二分法
+		/*
+		public int hIndex(int[] citations)
+		{
+			int len = citations.length;
+			int lo = 0, hi = len - 1;
+			while (lo <= hi)
+			{
+				int med = (hi + lo) / 2;
+				if (citations[med] == len - med)
+				{
+					return len - med;
+				}
+				else if (citations[med] < len - med)
+				{
+					lo = med + 1;
+				}
+				else
+				{
+					//(citations[med] > len-med), med qualified as a hIndex,
+					// but we have to continue to search for a higher one.
+					hi = med - 1;
+				}
+			}
+			return len - lo;
+		}
+		*/
 	}
 }
