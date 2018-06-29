a4bea4008aa5d70541d1b38521e43a703c42907a
zhangsw <zhangsw1412.cpp@gmail.com>
2018-06-29 19:56:26 +0800

    Counting Bits简单版.

--- src/main/java/leetcode/medium/CountingBits.java
+++ src/main/java/leetcode/medium/CountingBits.java
@@ -21,5 +21,21 @@ public class CountingBits
             }
             return result;
         }
+
+        /*
+        简洁的思路
+        f[i] = f[i / 2] + i % 2
+         */
+        /*
+        public int[] countBits(int num)
+        {
+            int[] f = new int[num + 1];
+            for (int i = 1; i <= num; i++)
+            {
+                f[i] = f[i >> 1] + (i & 1);
+            }
+            return f;
+        }
+        */
     }
 }
