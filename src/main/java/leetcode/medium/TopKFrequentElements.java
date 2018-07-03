91b9cf85e16bb5cb7d75c52f3f3047aacea8de62
zhangsw <zhangsw1412.cpp@gmail.com>
2018-07-03 16:49:08 +0800

    Top K Frequent Elements桶排序方法.

--- src/main/java/leetcode/medium/TopKFrequentElements.java
+++ src/main/java/leetcode/medium/TopKFrequentElements.java
@@ -2,6 +2,7 @@ package leetcode.medium;
 
 import static java.util.stream.Collectors.toList;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +24,7 @@ public class TopKFrequentElements
             {
                 hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
             }
+
             PriorityQueue<Entry<Integer, Integer>> priorityQueue = new PriorityQueue<>(k + 1, Comparator.comparingInt(Entry::getValue));
             for (Entry<Integer, Integer> entry : hashMap.entrySet())
             {
@@ -32,7 +34,41 @@ public class TopKFrequentElements
                     priorityQueue.poll();
                 }
             }
+
             return priorityQueue.stream().map(Entry::getKey).collect(toList());
         }
+
+        //桶排序
+        /*
+        public List<Integer> topKFrequent(int[] nums, int k)
+        {
+            HashMap<Integer, Integer> hashMap = new HashMap<>();
+            for (int num : nums)
+            {
+                hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+            }
+
+            List<Integer>[] bucket = new List[nums.length + 1];
+            for (Entry<Integer, Integer> entry : hashMap.entrySet())
+            {
+                int frequency = entry.getValue();
+                if (bucket[frequency] == null)
+                {
+                    bucket[frequency] = new ArrayList<>();
+                }
+                bucket[frequency].add(entry.getKey());
+            }
+
+            List<Integer> result = new ArrayList<>();
+            for (int i = bucket.length - 1; result.size() < k && i >= 0; i--)
+            {
+                if (bucket[i] != null)
+                {
+                    result.addAll(bucket[i]);
+                }
+            }
+            return result;
+        }
+        */
     }
 }
