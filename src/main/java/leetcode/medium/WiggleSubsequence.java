241be1828d773d9841bfa8a9023994525547e9a3
zhangsw <zhangsw1412.cpp@gmail.com>
2018-07-06 19:25:19 +0800

    Wiggle Subsequence.

---
+++ src/main/java/leetcode/medium/WiggleSubsequence.java
@@ -0,0 +1,44 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+/**
+ * Created by zhangsw on 2018/7/6.
+ */
+public class WiggleSubsequence
+{
+    class Solution
+    {
+        /*
+        动态规划
+        DP法，对于从index=0到当前位置index=i这段，用dp[i]表示最大的wiggle sequence长度，这个长度肯定是由前面某个位置+1得到的，
+        而前面某个位置对应的差值数组中的元素可能为正，也可能为负，
+        所以需要两组dp来记录，分别表示当前差值为正和当前差值为负所能取到的最大长度
+        */
+        public int wiggleMaxLength(int[] nums)
+        {
+            if (nums == null || nums.length == 0)
+            {
+                return 0;
+            }
+            int[] increase = new int[nums.length], decrease = new int[nums.length];
+            Arrays.fill(increase, 1);
+            Arrays.fill(decrease, 1);
+            for (int i = 0; i < nums.length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        increase[i] = Math.max(increase[i], decrease[j] + 1);
+                    }
+                    else if (nums[i] < nums[j])
+                    {
+                        decrease[i] = Math.max(decrease[i], increase[j] + 1);
+                    }
+                }
+            }
+            return Math.max(increase[nums.length - 1], decrease[nums.length - 1]);
+        }
+    }
+}
