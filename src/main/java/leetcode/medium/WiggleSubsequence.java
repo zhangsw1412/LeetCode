9e810b8f0f454986d62608030de6d4024057d6e7
zhangsw <zhangsw1412.cpp@gmail.com>
2018-07-06 19:31:14 +0800

    Wiggle Subsequence贪心法.

--- src/main/java/leetcode/medium/WiggleSubsequence.java
+++ src/main/java/leetcode/medium/WiggleSubsequence.java
@@ -40,5 +40,33 @@ public class WiggleSubsequence
             }
             return Math.max(increase[nums.length - 1], decrease[nums.length - 1]);
         }
+
+        /*
+        贪心法
+        维护两个变量p和q，然后遍历数组，如果当前数字比前一个数字大，则p=q+1，
+        如果比前一个数字小，则q=p+1，最后取p和q中的较大值跟n比较，取较小的那个
+         */
+        /*
+        public int wiggleMaxLength(int[] nums)
+        {
+            if (nums == null || nums.length == 0)
+            {
+                return 0;
+            }
+            int increase = 1, decrease = 1;
+            for (int i = 1; i < nums.length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    increase = decrease + 1;
+                }
+                else if (nums[i] < nums[i - 1])
+                {
+                    decrease = increase + 1;
+                }
+            }
+            return Math.min(nums.length, Math.max(increase, decrease));
+        }
+        */
     }
 }
